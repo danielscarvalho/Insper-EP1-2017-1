10d51a17759b3be37b01a2035ecb831d2dc898ac
Hilton Fernandes <hilton.g.fernandes@gmail.com>
2017-03-21 10:59:19 -0300

    Comentando versão de iniciante

--- ep1-iniciante.py
+++ ep1-iniciante.py
@@ -8,17 +8,24 @@ with open("nomes.txt", "r") as arquivo:
     for linha in arquivo:
         lista_de_nomes.append (linha)
 
+# Lista de conectivos conhecidos
 conectivos = ['Da', 'Das', 'De', 'Do', 'Dos']
 
 # Percorre lista de nomes, titulando e ajustando conectivos
 lista_titulada = []
 for nome in lista_de_nomes:
+    # Titula os nomes -- tudo em minúsculas, exceto a inicial
     nome = nome.title ()
+    
+    # Quebra o nome completo em palavras distintas
     nomes = nome.split ()
     for ind in range (len (nomes)):
+        # Se uma das palavras do nome for um conectivo
         if nomes[ind] in conectivos:
+            # Minuscula o nome
             nomes[ind] = nomes[ind].lower ()
     
+    # Acrescenta o nome tratado à lista de nomes titulados
     lista_titulada.append (" ".join (nomes))
 
 # Ordena lista de nomes
